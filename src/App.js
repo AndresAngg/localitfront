d2d66d3b77f1dcd24839bf17df0a74ea12c04482
AndresHelloWorld <andresangg@gmail.com>
2021-10-03 21:50:42 -0500

    add update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import ReactMapGL, { NavigationControl, GeolocateControl } from 'react-map-gl';
+
+require('dotenv').config()
+
 
 function App() {
+  const [viewport, setViewport] = useState({
+    width: "60vw",
+    height: "50vh",
+    latitude: 11.001874,
+    longitude: -74.817420,
+    zoom: 13
+  });
+  const navControlSt = {
+    right: 10,
+    top: 10
+  }
+  const geolocateControlStyle= {
+    right: 10,
+    top: 10
+  };
+  console.log('hola')
   return (
-    <div className="App">
-      <div id="map" className="map-cont"></div>
+    <div className="cont">
+      <div id="form">
+
+        <h1>Localit-x</h1>
+        <div className="inputss">
+          <input type="text" placeholder="Inicio" className="punto1" />
+          <input type="text" placeholder="Fin" className="punto2" />
+          <input value="Buscar ruta" type="button" className="btnn" />
+        </div>
+      </div>
+      <div className="containermap">
+
+        <ReactMapGL
+          {...viewport}
+          mapboxApiAccessToken={'pk.eyJ1IjoiYW5kcmVzMjkiLCJhIjoiY2t1YnFoZ3lnMDlwZDJ2bzk1NGY5MXFnaCJ9.I59-MloYuDI6DVGutPiEyQ'}
+          mapStyle="mapbox://styles/mapbox/streets-v11"
+          onViewportChange={nextViewport => setViewport(nextViewport)}
+          NavegationControl
+        >
+         
+          <NavigationControl style={navControlSt} />
+          <GeolocateControl
+            style={geolocateControlStyle}
+            positionOptions={{ enableHighAccuracy: true }}
+            trackUserLocation={true}
+            auto
+          />
+        </ReactMapGL>
+      </div>
+
     </div>
   );
+
 }
 
 export default App;
---
+++ src/services/mapCustom.service.js
@@ -0,0 +1,13 @@
+import 'react'
+import * as mapboxgl from 'mapbox-gl'
+require ('dotenv').config()
+
+class Map extends React.Component {
+    
+
+    constructor(props) {
+    }
+
+
+}
+export default Map
